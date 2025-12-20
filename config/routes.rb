e7167120d923875ac4c9fe6fc6dfae15858792b4
mitsu <maronmituki0326@gmail.com>
2025-12-20 17:08:25 +0900

    ルーティング一時完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,33 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "top#index"
+  #未ログイン状態での商品検索
+  resources :items,  only: [:index, :show] do
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :session, only: [:create, :destroy]
+  resource :account, except: [:destroy] do
+    resource :orders, only: [:index, :show]
+  end
+  resource :password, only: [:show, :edit, :update]
+  resource :cart, only: [:new,:show]
+
+  #注文明細一覧ページ用
+  resources :details, only: [:index]
+  
+  #店員のルーティング
+  namespace :staff do
+    root "top#index"
+    resources :orders, only: [:index, :show ] do
+    end
+    resources :stocks, only: [:index, :show]
+  end
+  
+  namespace :admin do
+    root "top#index"
+    resources :members , only: [:edit, :show]
+    resources :stocks 
+    resources :items do
+      get "sales" ,on: :collection
+    end
+  end
 end
