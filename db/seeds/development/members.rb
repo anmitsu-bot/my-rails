69aa7314653f8920dd700c62cf7353a7dfa2f797
mitsu <maronmituki0326@gmail.com>
2025-12-20 23:44:54 +0900

    css導入

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -5,3 +5,13 @@ Member.create(
     password: "asagao!",
     password_confirmation: "asagao!"
 )
+
+0.upto(4) do |idx|
+    Member.create(
+        name: idx,
+        point:  0,
+        role:  (idx % 2) + 1 ,
+        password: "asagao!",
+        password_confirmation: "asagao!"
+    )
+end
