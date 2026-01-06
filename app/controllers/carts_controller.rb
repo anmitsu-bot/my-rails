f235d6b3efc10f227900c96399b566b70c9e2f3b
mitsu <maronmituki0326@gmail.com>
2026-01-06 21:54:13 +0900

    confirmアクション実装、次は注文一覧、注文明細一覧を作成予定

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -16,18 +16,49 @@ class CartsController < ApplicationController
     @cart  = session[:cart] || {}
     @items = Item.where(id: session[:cart].keys)
     @sum = 0
-    @use_point = false
+    @use_point = false #ポイント使うか否か　あとでラジオボタンで実装
+    @point_amount = 0 #使うポイント数　formをインスタンスで管理すれば楽なため
   end
 
-  def confirm
+  def confirm #注文確定 orderとdetailを作る
+    @sum = 0
+    @items = Item.where(id: session[:cart].keys)
 
+    @member = current_member
+    
+    #orderのレコード作成
+    @order = Order.new()
+    @order.reserver = current_member
+    @order.use_point = params[:mypoint]
+    if @order.save
+      #detailのレコード作成
+       @items.each do |item|
+         @detail = Detail.new()
+         @detail.bought = item
+         @detail.base = @order
+         @detail.number = session[:cart][item.id]
+
+         if @detail.save && session[:cart][item.id] != 0
+          @sum = @sum + item.price*session[:cart][item.id]
+         else
+          render "show"
+         end
+       end
+       redirect_to :root, notice: "注文が完了しました。"
+    end
+    @order.amount = @sum - @order.use_point
+    @order.save
+
+    @member.point = @member.point - params[:mypoint]
+    @member.save
+
+    #カートを初期化
+    session[:cart] ||= {}
+    session[:cart][item_id] ||= 0
   end
+
   def zero
-    session[:cart][params[:item_id]] = 0
-
+    session[:cart][params[:item_id]] = 0 #指定した商品の個数をゼロに
   end
-
-  def destroy
-
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
     resource :orders, only: [:index, :show]
   end
   resource :password, only: [:show, :edit, :update]
-  resource :cart, only: [:show, :destroy] do
+  resource :cart, only: [:show] do
     post :add
     post :zero
-    get "confirm" , on: :collection
+    get "confirm" 
   end
 
   get "login" => "top#login"
