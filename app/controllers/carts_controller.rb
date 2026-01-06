1804eb658ef7acba504d051dbf4efa36fbd58250
mitsu <maronmituki0326@gmail.com>
2026-01-06 14:24:06 +0900

    カートshowアクション完成

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,13 @@
 class CartsController < ApplicationController
   def add
-    # カートが未定義なら0をデフォルト値とするHashで初期化（キーは商品ID、値は数量）
-    session[:cart] ||= Hash.new(0)
 
     item_id  = params[:item_id]
     quantity = params[:quantity].to_i
+
+    # カートが未定義なら0をデフォルト値とするHashで初期化（キーは商品ID、値は数量）
+    session[:cart] ||= {}
+    session[:cart][item_id] ||= 0
+
     # カートの商品IDに対する数量を追加
     session[:cart][item_id] += quantity
     redirect_to :items, notice: "商品をカートに追加しました。"
