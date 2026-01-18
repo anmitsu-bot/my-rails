4db0bd95b42842523ef385bb7e9a39a2bdd1c28c
mitsu <maronmituki0326@gmail.com>
2026-01-18 18:39:55 +0900

    メニューバー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,32 @@
 class SessionsController < ApplicationController
-    def create
-        member = Member.find_by(name: params[:name])
-        if member&.authenticate(params[:password])
-            session[:member_id] = member.id
-        else
-            flash.alert = "名前とパスワードが一致しません"
-        end
-        redirect_to :root
-    end
+  def create
+    member = Member.find_by(name: params[:name])
 
-    def destroy
-        session.delete(:member_id)
-        session[:cart] = {}
-        redirect_to :root
+    if member&.authenticate(params[:password])
+      session[:member_id] = member.id
+      redirect_to after_login_path(member)
+    else
+      flash.alert = "名前とパスワードが一致しません"
+      redirect_to :login
     end
+  end
+
+  def destroy
+    session.delete(:member_id)
+    session[:cart] = {}
+    redirect_to :root
+  end
+
+  private
+
+  def after_login_path(member)
+    case member.role
+    when 3
+      :admin_root
+    when 2
+      :staff_root
+    else
+      :root
+    end
+  end
 end
